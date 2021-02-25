5c31ca4effd4c41263c3645da83ddf28816a871e
Peter Plantinga <plantinga.peter@protonmail.com>
2021-02-25 17:52:56 -0500

    Working version

---
+++ ogi_prepare.py
@@ -0,0 +1,194 @@
+"""
+Data preparation for OGI kids' speech corpus.
+
+Authors
+* Peter Plantinga 2020
+"""
+
+import os
+import re
+import json
+import logging
+from glob import glob
+from g2p_en import G2p
+from speechbrain.utils.data_utils import get_all_files
+from speechbrain.dataio.dataio import read_audio
+
+G2P = G2p()
+NOT_PHONEMES = [" ", ".", "!", "?"]
+logger = logging.getLogger(__name__)
+SAMPLERATE = 16000
+
+
+def prepare_ogi(
+    data_folder, train_file, valid_file, test_file, valid_frac=0.03, test_frac=0.03,
+):
+    """
+    Prepares the json files for the CSLU dataset.
+
+    Arguments
+    ---------
+    data_folder : str
+        Path to the folder where the original CSLU dataset is stored.
+    train_file : str
+        Path for storing the train data manifest file.
+    valid_file : str
+        Path for storing the validation data manifest file.
+    test_file : str
+        Path for storing the test data manifest file.
+    valid_frac : float
+        Approximate fraction of speakers to assign to valid data in each grade.
+    test_frac : float
+        Approximate fraction of speakers to assign to test data in each grade.
+    """
+    # Setting file extension.
+    extension = [".wav"]
+
+    # Check if this phase is already done (if so, skip it)
+    if skip(train_file, valid_file, test_file):
+        logger.debug("Skipping preparation, completed in previous run.")
+        return
+
+    logger.debug("Creating json files for the OGI Dataset...")
+
+    speech_dir = os.path.join(data_folder, "speech", "scripted")
+    grade_list = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
+
+    # Creating json file for training data
+    wav_lst_train = []
+    wav_lst_valid = []
+    wav_lst_test = []
+    for grade in grade_list:
+        grade_dir = os.path.join(speech_dir, grade)
+
+        # Collect list of folders for distinct speakers
+        spk_list = sorted(glob(os.path.join(grade_dir, "*", "ks*")))
+
+        # Divide into sublists
+        valid_len = int(valid_frac * len(spk_list))
+        test_len = int(test_frac * len(spk_list))
+        valid_spk_list = spk_list[:valid_len]
+        test_spk_list = spk_list[valid_len : valid_len + test_len]
+        train_spk_list = spk_list[valid_len + test_len :]
+
+        # Find files and add em
+        for folder in train_spk_list:
+            wav_lst_train.extend(get_all_files(folder, match_and=extension))
+        for folder in valid_spk_list:
+            wav_lst_valid.extend(get_all_files(folder, match_and=extension))
+        for folder in test_spk_list:
+            wav_lst_test.extend(get_all_files(folder, match_and=extension))
+
+    # Create data maps
+    id2word = load_id2word_map(data_folder)
+    id2verify = load_id2verify_map(data_folder, grade_list)
+
+    # Create json with all files
+    create_json(wav_lst_train, train_file, id2word, id2verify)
+    create_json(wav_lst_valid, valid_file, id2word, id2verify)
+    create_json(wav_lst_test, test_file, id2word, id2verify)
+
+
+def skip(*filenames):
+    """
+    Detects if the data_preparation has been already done.
+    If the preparation has been done, we can skip it.
+
+    Returns
+    -------
+    bool
+        if True, the preparation phase can be skipped.
+        if False, it must be done.
+    """
+    # Checking json files
+    for json in filenames:
+        if not os.path.isfile(json):
+            return False
+
+    return True
+
+
+def load_id2word_map(data_folder):
+    """Load map from word id to characters"""
+    word_file = os.path.join(data_folder, "docs", "all.map")
+    id2word = {}
+    for line in open(word_file):
+        line = line.split()
+        if len(line) == 0:
+            continue
+        word_id = line[0]
+
+        # Normalize and store
+        words = " ".join(line[1:]).lower()
+        words = re.sub(r"[^a-z ]", "", words)
+        id2word[word_id] = words
+
+    return id2word
+
+
+def load_id2verify_map(data_folder, grades):
+    id2verify = {}
+    for grade in grades:
+        verify_file = os.path.join(data_folder, "docs", grade + "-verified.txt")
+        for line in open(verify_file):
+            filename, verify = line.split()
+            utterance_id = filename[-12:-4]
+            id2verify[utterance_id] = int(verify)
+
+    return id2verify
+
+
+def words2phonemes(word):
+    "Convert all words to phonemes"
+    return " ".join(p.strip("012") for p in G2P(word) if p not in NOT_PHONEMES)
+
+
+def create_json(wav_lst, json_file, id2word, id2verify):
+    """
+    Creates the json file given a list of wav files.
+
+    Arguments
+    ---------
+    wav_lst : list
+        The list of wav files of a given data split.
+    json_file : str
+        The path of the output json file
+    id2word : dict
+        A mapping from id to word
+    """
+
+    logger.debug(f"Creating json manifest list: {json_file}")
+    json_dict = {}
+
+    # Processing all the wav files in the list
+    for wav_file in wav_lst:
+
+        path = os.path.normpath(wav_file).split(os.path.sep)
+        relative_filename = os.path.join("{root}", *path[-6:])
+
+        # Compute phoneme targets
+        snt_id = wav_file[-12:-4]
+        word_id = wav_file[-7:-5].upper()
+        words = id2word[word_id]
+        phonemes = words2phonemes(words)
+
+        # Don't use bad quality recordings
+        if id2verify.get(snt_id, 1) == 3:
+            continue
+
+        # Reading the signal (to retrieve duration in seconds)
+        signal = read_audio(wav_file)
+        duration = signal.shape[0] / SAMPLERATE
+
+        json_dict[snt_id] = {
+            "wav": relative_filename,
+            "length": duration,
+            "words": words,
+            "phonemes": phonemes,
+        }
+
+    # Writing the json lines
+    with open(json_file, mode="w") as json_f:
+        json.dump(json_dict, json_f, indent=2)
+
+    logger.debug(f"{json_file} successfully created!")
---
+++ train.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+"""Recipe for training a recognizer on kids' speech.
+
+To run this recipe, do the following:
+> python train.py hparams/train.yaml --data_folder /path/to/ogi
+
+Authors
+ * Peter Plantinga 2021
+"""
+import sys
+import torch
+import speechbrain as sb
+from ogi_prepare import prepare_ogi
+from hyperpyyaml import load_hyperpyyaml
+from speechbrain.utils.distributed import run_on_main
+
+
+# Define training procedure
+class ASR(sb.core.Brain):
+    def compute_forward(self, batch, stage):
+        batch = batch.to(self.device)
+        wavs, wav_lens = batch.sig
+
+        # Adding time-domain SpecAugment if specified
+        if hasattr(self.hparams, "augmentation") and stage == sb.Stage.TRAIN:
+            wavs = self.hparams.augmentation(wavs, wav_lens)
+
+        feats = self.hparams.compute_features(wavs)
+        feats = self.hparams.time_reduce(feats)
+        feats = self.hparams.normalize(feats, wav_lens)
+        out = self.modules.recognizer(feats)
+        out = self.modules.recognizer_output(out)
+        logprobs = self.hparams.log_softmax(out)
+
+        return logprobs
+
+    def compute_objectives(self, predictions, batch, stage):
+        wavs, wav_lens = batch.sig
+        phonemes, phoneme_lens = batch.tokens
+
+        if stage != sb.Stage.TRAIN:
+            decoded = sb.decoders.ctc_greedy_decode(
+                predictions, wav_lens, blank_id=self.encoder.get_blank_index()
+            )
+            self.per_metrics.append(
+                ids=batch.id,
+                predict=decoded,
+                target=phonemes,
+                target_len=phoneme_lens,
+                ind2lab=self.encoder.decode_ndim,
+            )
+
+        ctc_loss = sb.nnet.losses.ctc_loss(
+            log_probs=predictions,
+            targets=phonemes,
+            input_lens=wav_lens,
+            target_lens=phoneme_lens,
+            blank_index=self.encoder.get_blank_index(),
+        )
+        return ctc_loss
+
+    def on_stage_start(self, stage, epoch):
+        if stage != sb.Stage.TRAIN:
+            self.per_metrics = self.hparams.per_computer()
+
+    def on_stage_end(self, stage, stage_loss, epoch):
+        if stage == sb.Stage.TRAIN:
+            self.train_loss = stage_loss
+        else:
+            per = self.per_metrics.summarize("error_rate")
+            stage_stats = {
+                "loss": stage_loss,
+                "PER": per,
+            }
+
+        if stage == sb.Stage.VALID:
+            old_lr, new_lr = self.hparams.lr_annealing(per)
+            epoch_stats = {"epoch": epoch, "lr": old_lr}
+            self.hparams.train_logger.log_stats(
+                epoch_stats, {"loss": self.train_loss}, stage_stats
+            )
+            self.checkpointer.save_and_keep_only(
+                meta=stage_stats, min_keys=["PER"],
+            )
+        elif stage == sb.Stage.TEST:
+            self.hparams.train_logger.log_stats(
+                {"Epoch loaded": self.hparams.epoch_counter.current},
+                test_stats=stage_stats,
+            )
+            with open(self.hparams.test_stats_file, "w") as w:
+                self.per_metrics.write_stats(w)
+
+
+def dataio_prep(hparams):
+    """Creates datasets and loading pipelines"""
+
+    encoder = sb.dataio.encoder.CTCTextEncoder(blank_label="<blank>")
+
+    @sb.utils.data_pipeline.takes("wav")
+    @sb.utils.data_pipeline.provides("sig")
+    def audio_pipeline(wav):
+        return sb.dataio.dataio.read_audio(wav)
+
+    @sb.utils.data_pipeline.takes("phonemes")
+    @sb.utils.data_pipeline.provides("tokens_list", "tokens")
+    def text_pipeline(phonemes):
+        tokens_list = phonemes.strip().split()
+        yield tokens_list
+        tokens = encoder.encode_sequence(tokens_list)
+        yield torch.LongTensor(tokens)
+
+    data = {}
+    for dataset in ["train", "valid", "test"]:
+        data[dataset] = sb.dataio.dataset.DynamicItemDataset.from_json(
+            json_path=hparams[f"{dataset}_annotation"],
+            replacements={"root": hparams["data_folder"]},
+            dynamic_items=[audio_pipeline, text_pipeline],
+            output_keys=["id", "sig", "tokens"],
+        ).filtered_sorted(sort_key="length")
+
+    encoder.update_from_didataset(data["train"], output_key="tokens_list")
+
+    return data, encoder
+
+
+if __name__ == "__main__":
+    # Load hyperparameters file with command-line overrides
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+
+    # Create experiment directory
+    sb.core.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
+        overrides=overrides,
+    )
+
+    # Prepare data
+    run_on_main(
+        prepare_ogi,
+        kwargs={
+            "data_folder": hparams["data_folder"],
+            "train_file": hparams["train_annotation"],
+            "valid_file": hparams["valid_annotation"],
+            "test_file": hparams["test_annotation"],
+        },
+    )
+
+    datasets, encoder = dataio_prep(hparams)
+
+    # Create brain object for training
+    asr_brain = ASR(
+        modules=hparams["modules"],
+        opt_class=hparams["opt_class"],
+        hparams=hparams,
+        run_opts=run_opts,
+        checkpointer=hparams["checkpointer"],
+    )
+    asr_brain.encoder = encoder
+
+    asr_brain.fit(
+        epoch_counter=asr_brain.hparams.epoch_counter,
+        train_set=datasets["train"],
+        valid_set=datasets["valid"],
+        train_loader_kwargs=hparams["dataloader_opts"],
+        valid_loader_kwargs=hparams["dataloader_opts"],
+    )
+
+    asr_brain.evaluate(
+        test_set=datasets["test"],
+        min_key="PER",
+        test_loader_kwargs=hparams["dataloader_opts"],
+    )
